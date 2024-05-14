177709ed1142b2d177a1cc7519bd1ef7c83cfb2e
Md-Tohin <mdtohin8585@gmail.com>
2024-05-14 14:48:42 +0600

    User && Cart Related API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,48 +16,82 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db("bistroBossRestaurantDB").collection("products");
-    const reviewCollection = client.db("bistroBossRestaurantDB").collection("reviews");
-    const menuCollection = client.db("bistroBossRestaurantDB").collection("menu");
-    const cartCollection = client.db("bistroBossRestaurantDB").collection("carts");
+    const userCollection = client
+    .db("bistroBossRestaurantDB")
+    .collection("users");
+    const productCollection = client
+      .db("bistroBossRestaurantDB")
+      .collection("products");
+    const reviewCollection = client
+      .db("bistroBossRestaurantDB")
+      .collection("reviews");
+    const menuCollection = client
+      .db("bistroBossRestaurantDB")
+      .collection("menu");
+    const cartCollection = client
+      .db("bistroBossRestaurantDB")
+      .collection("carts");
 
-    app.get('/api/reviews', async(req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-    });
-
-    app.get('/api/menu', async(req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-    });
-
-    //  get cart items
-    app.get('/api/carts', async(req, res) => {
-      const email = req.query.email;
-      const query = {email: email};
-      const result = await cartCollection.find(query).toArray();
+    //  reviews related api
+    app.get("/api/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
       res.send(result);
     });
 
-    //  store cart item
-    app.post('/api/carts', async(req, res) => {
-      const cartItem = req.body;
-      const result = await cartCollection.insertOne(cartItem);
+    //  menu related api
+    app.get("/api/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    //  user create
+    app.post("/api/users", async(req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExistsEmail = await userCollection.findOne(query);
+      if (isExistsEmail) {
+        return res.send({message: "User email already exists", insertedId: null});
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     })
 
-
+    //  get cart items
+    app.get("/api/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    //  store cart item
+    app.post("/api/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    //  cart item delete
+    app.delete("/api/cart/:id/:email", async (req, res) => {
+      const id = req.params.id;
+      const email = req.params.email;
+      const query = {
+        $and: [{ email: email }, { _id: new ObjectId(id) }],
+      };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -66,10 +100,10 @@ async function run() {
 run().catch(console.dir);
 
 // api
-app.get('/', (req, res) => {
-    res.send("Module 64 Server");
+app.get("/", (req, res) => {
+  res.send("Module 64 Server");
 });
 
 app.listen(port, () => {
-    console.log(`Module 64 server is running on port ${port}`);
+  console.log(`Module 64 server is running on port ${port}`);
 });
